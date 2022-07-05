71351e14ce645c5dde152071feb8bc09cd32ce97
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-07-05 11:55:04 +0200

    Added restaurantsDAO

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/restaurants", restaurants);
-app.use("*", (req, res) => res.status(404).json({ message: "Not Found" }));
+app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
 export default app;
