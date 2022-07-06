398e84338056be6d1c4189651d635eb384976848
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-07-06 15:20:41 +0200

    Bugfixed?

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use("/api/restaurants", restaurants);
+app.get("/api/restaurants", restaurants);
 app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
 export default app;
