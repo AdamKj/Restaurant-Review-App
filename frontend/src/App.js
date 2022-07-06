5f9be546b2a9254cecf496978e7cef8fe46d66ca
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-07-06 12:00:27 +0200

    Bugfixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import { Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from "./components/add-review";
+import Login from "./components/login";
+import Restaurant from "./components/restaurant";
+import RestaurantsList from "./components/restaurants-list";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" className="navbar-brand">
+          Restaurant Reviews
         </a>
-      </header>
-    </div>
+        <div className="navbar-nav me-auto">
+          <li className="nav-item">
+            <Link to={Restaurant} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item">
+            {user ? (
+              <a onClick={logout} className="nav-link">
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={Login} className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="conatiner mt-3">
+        <Router>
+          <Route path={["/", "/restaurants"]} component={RestaurantsList} />
+          <Route
+            path="/restaurants/:id/review"
+            render={(props) => <AddReview {...props} user={user} />}
+          />
+          <Route
+            path="/restaurants/:id"
+            render={(props) => <Restaurant {...props} user={user} />}
+          />
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} login={login} />}
+          />
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RestaurantList() {
+  return <div className="App">Hello World</div>;
+}
+
+export default RestaurantList;
